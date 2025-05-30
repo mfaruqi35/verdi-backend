2f4d9ed373c1e802e6403d628b91a8e5f91d12c3
mhdfaruqi23 <mfaruqi23@mhs.usk.ac.id>
2025-05-30 16:45:30 +0700

    deploy

--- server.js
+++ server.js
@@ -14,10 +14,15 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to MongoDB"));
 
-app.use("/user", userRouter);
 app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.use("/user", userRouter);
 
 app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
 
